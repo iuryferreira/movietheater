ee4e7f60aab721af5316d4572d5c4db59a85abcc
Iury Ferreira <iury.franklinferreira@gmail.com>
2021-12-22 08:59:19 -0300

    refactor: change seat name

--- src/MovieTheater.Api/RoomFactory.cs
+++ src/MovieTheater.Api/RoomFactory.cs
@@ -7,7 +7,7 @@ public static class RoomFactory
     public static Room Create()
     {
         List<Seat> seats = new();
-        for (var index = 1; index <= 10; index++) seats.Add(new Seat {Number = $"SEAT_{index}", Busy = false});
+        for (var index = 1; index <= 10; index++) seats.Add(new Seat {Number = $"S{index}", Busy = false});
         return new Room {Seats = seats, Bookings = new List<Booking>()};
     }
 }
